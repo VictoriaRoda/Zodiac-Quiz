6ef0756e18debc2822fa9d2709ceb399daa713a5
Victoria <62159005+VictoriaRoda@users.noreply.github.com>
2020-07-31 06:01:58 -0400

    Upload CSS and JS

---
+++ store.js
@@ -0,0 +1,116 @@
+const STORE = {
+  questions: [//1
+    {
+      question: "March 5th is your best friends birthday. What is his zodiac sign?",
+      options: [
+        "Cancer", 
+        "Virgo", 
+        "Pisces", 
+        "Aries"
+      ],
+      answer: "Pisces"
+    },
+    //2
+    {
+      question: "Your mom got a tattoo of a bull. What zodiac sign is she representing?",
+      options: [
+        "Aries",
+        "Saggitarius", 
+        "Capricorn",
+        "Taurus"
+      ],
+      answer: "Taurus"
+    },
+    //3
+    {
+      question: "The zodiac was originally used to track which of the following calendars?",
+      options: [
+        "Agricultural", 
+        "Lunar", 
+        "Solar", 
+        "Seasonal"
+      ],
+      answer: "Agricultural"
+    },
+    //4
+    {
+      question: "Gemini, Libra, and Aquarius hail from the same element. Pick that element.", 
+      options: [
+        "Earth", 
+        "Water", 
+        "Fire",
+        "Air"
+        ],
+      answer: "Air"
+    },
+    //5
+    {
+      question: "Birthdays just before or after the zodiac changes are on the cusp or having traits from two signs. What duo is playing Happy Birthday between the dates of June 17th and June 23rd?",
+      options: [
+        "Taurus/Gemini", 
+        "Gemini/Cancer", 
+        "Gemini/Leo", 
+        "Leo/Virgo"
+      ],
+      answer: "Gemini/Cancer"
+    },
+    //6
+    {
+      question: "The Chinese zodiac is based on an ancient story about animals racing to the Buddah to become part of the zodiac. 3 animals crossed first. The Rat, the Tiger, and ... ?",
+      options: [
+        "Rooster", 
+        "Ox", 
+        "Dragon", 
+        "Pig"
+      ],
+      answer: "Ox"
+    },
+    //7
+    {
+      question: "Which animal was last in The Great Race?",
+      options: [
+        "Dog", 
+        "Monkey", 
+        "Snake", 
+        "Pig"
+      ],
+      answer: "Pig"
+    },
+    //8
+    {
+      question: "Chinese zodiac cycles annually. What years are represented by The Monkey?", 
+      options: [
+        "1968, 1980, 1992, 2004, 2016, 2028", 
+        "1948, 1960, 1972, 1984, 1996, 2008", 
+        "1969, 1979, 1993, 2005, 2015, 2027", 
+        "1971, 1983, 1995, 2007, 2019, 2031"
+      ],
+      answer: "1968, 1980, 1992, 2004, 2016, 2028"
+    },
+    //9
+    {
+      question: "Chinese zodiac elements are a little different than the western style. Pick the Chinese elements.",
+      options: [
+        "Air, Earth, Fire, Wind, Wood", 
+        "Gold, Water, Wood, Earth, Fire", 
+        "Silver, Fire, Earth, Gold, Air", 
+        "Silver, Water, Wood, Fire, Air"
+      ], 
+      answer: "Gold, Water, Wood, Earth, Fire"
+    },
+    //10
+    {
+      question: "Chinese zodiac changes annully, but which day?",
+      options: [
+        "December 30th", 
+        "September 25th", 
+        "January 25th", 
+        "June 21st"
+      ],
+      answer: "January 25th"
+    },
+    
+  ],
+  currentQuestion: 0,
+  score: 0
+};
